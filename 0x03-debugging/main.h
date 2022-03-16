16b938499e52aede635a87aa15f00c3a15ac0617
remi_oni <93938817+Oni-Jremi@users.noreply.github.com>
2022-03-16 15:50:05 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,5 @@
-/* #ifndef MAIN_H
-* #define MAIN_H
-*/
+#ifndef __MAIN_H__
+#define __MAIN_H__
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -10,4 +9,4 @@ void print_remaining_days(int month, int day, int year);
 
 int convert_day(int month, int day);
 
-/*#endif MAIN_H*/
+#endif __MAIN_H__
