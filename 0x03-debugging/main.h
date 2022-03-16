0c43dc242d659105f92a65a738e32d1121967608
remi_oni <93938817+Oni-Jremi@users.noreply.github.com>
2022-03-16 19:02:03 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,8 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
 
-void print_remaining_days(int month, int day, int year);
-
-int convert_day(int month, int day);
-
-#endif __MAIN_H__
+#endif /*MAIN_H*/
